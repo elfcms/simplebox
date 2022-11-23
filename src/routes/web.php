74ef01fa4a15b80836eb01728a90ce276d347a3c
Maxim Klassen <himmelmaler@gmail.com>
2022-11-23 11:54:16 +0100

    customizable admin panel path

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,15 +4,16 @@ use Elfcms\Simplebox\Models\SimpleboxDataType;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+$adminPath = Config::get('elfcms.basic.admin_path') ?? '/admin';
 
-Route::group(['middleware'=>['web','cookie','start']],function(){
+Route::group(['middleware'=>['web','cookie','start']],function() use ($adminPath) {
 
-    Route::name('admin.')->middleware('admin')->group(function(){
+    Route::name('admin.')->middleware('admin')->group(function() use ($adminPath) {
 
-        Route::name('simplebox.')->group(function(){
-            Route::resource('/admin/simplebox/items', \Elfcms\Simplebox\Http\Controllers\Resources\SimpleboxItemController::class)->names(['index' => 'items']);
+        Route::name('simplebox.')->group(function() use ($adminPath) {
+            Route::resource($adminPath . '/simplebox/items', \Elfcms\Simplebox\Http\Controllers\Resources\SimpleboxItemController::class)->names(['index' => 'items']);
         });
-        Route::get('/admin/ajax/json/simplebox/datatypes',function(Request $request){
+        Route::get($adminPath . '/ajax/json/simplebox/datatypes',function(Request $request){
             $result = [];
             if ($request->ajax()) {
                 $result = SimpleboxDataType::all()->toArray();
